e53b6079b74b66cb442904590e5834182b28d194
steve <stevesiu1013@gmail.com>
2025-04-04 19:07:31 +1100

    fix

--- csrc/flash_attn/src/flash_fwd_kernel.cu
+++ csrc/flash_attn/src/flash_fwd_kernel.cu
@@ -408,6 +408,6 @@ flash_fwd(torch::Tensor q,
                                             o_ptr,
                                             l_ptr,
                                             batch_size, seq_len, num_heads, head_dim);
-    return {o};
+    return {o, l};
 
 }
