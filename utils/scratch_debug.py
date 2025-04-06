8bef3c66bfe832a06afe3d177ff61deb0bd0a44d
steve <stevesiu1013@gmail.com>
2025-04-06 15:01:45 +1000

    fix

---
+++ utils/scratch_debug.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn.functional as F
+from torch.profiler import profile, record_function, ProfilerActivity
+from flash_attn_turing import flash_attn_func
+import argparse
+
+
+q = torch.randn(2,3, dtype=torch.float16).to("cuda")
+k = torch.randn(2,3, dtype=torch.float16).to("cuda")
+v = torch.randn(2,3, dtype=torch.float16).to("cuda")
+
+a, b = flash_attn_func(q,k,v,1,1,1,1)
+print(a)
+print(b)
+
