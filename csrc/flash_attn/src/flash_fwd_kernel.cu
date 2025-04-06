22c216a48d276cad65b4062347405c18edfcb2bc
steve <stevesiu1013@gmail.com>
2025-04-06 14:53:22 +1000

    fix

--- csrc/flash_attn/src/flash_fwd_kernel.cu
+++ csrc/flash_attn/src/flash_fwd_kernel.cu
@@ -376,31 +376,31 @@ flash_fwd(torch::Tensor q,
             torch::Tensor v,
             int batch_size, int seq_len, int num_heads, int head_dim)
 {
-    constexpr int kBlockM = 128;
-    constexpr int kBlockN = 64;
-    constexpr int kHeadDim = 128;
+//     constexpr int kBlockM = 128;
+//     constexpr int kBlockN = 64;
+//     constexpr int kHeadDim = 128;
 
 //     auto device = q.device();
 
-    torch::Tensor o = torch::zeros(q.sizes(), q.options().dtype(torch::kFloat16));
-    torch::Tensor test = torch::zeros(q.sizes(), q.options().dtype(torch::kFloat16));
+//     torch::Tensor o = torch::zeros(q.sizes(), q.options().dtype(torch::kFloat16));
+//     torch::Tensor test = torch::zeros(q.sizes(), q.options().dtype(torch::kFloat16));
 
 //     std::vector<int64_t> size = {batch_size, seq_len, head_dim};
 //     torch::Tensor l = torch::empty(size, q.options().dtype(torch::kFloat32).device(device));
 
-    TORCH_CHECK(o.is_cuda(), "Tensor o is not on CUDA");
-    TORCH_CHECK(test.is_cuda(), "Tensor test is not on CUDA");
+//     TORCH_CHECK(o.is_cuda(), "Tensor o is not on CUDA");
+//     TORCH_CHECK(test.is_cuda(), "Tensor test is not on CUDA");
 
-    half_t* q_ptr = reinterpret_cast<half_t*>(q.data_ptr());
-    half_t* k_ptr = reinterpret_cast<half_t*>(k.data_ptr());
-    half_t* v_ptr = reinterpret_cast<half_t*>(v.data_ptr());
-    half_t* o_ptr = reinterpret_cast<half_t*>(o.data_ptr());
+//     half_t* q_ptr = reinterpret_cast<half_t*>(q.data_ptr());
+//     half_t* k_ptr = reinterpret_cast<half_t*>(k.data_ptr());
+//     half_t* v_ptr = reinterpret_cast<half_t*>(v.data_ptr());
+//     half_t* o_ptr = reinterpret_cast<half_t*>(o.data_ptr());
 
 //     float* l_ptr = reinterpret_cast<float*>(l.data_ptr());
 
-    dim3 dimGrid(batch_size, num_heads, seq_len / kBlockM);
-    dim3 dimBlock(256);
-    int maxbytes = 65536;
+//     dim3 dimGrid(batch_size, num_heads, seq_len / kBlockM);
+//     dim3 dimBlock(256);
+//     int maxbytes = 65536;
 
 
 
