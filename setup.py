0d47253ae889beef7ba5623d8015b056c3556a72
steve <stevesiu1013@gmail.com>
2025-04-06 17:45:59 +1000

    fix

--- setup.py
+++ setup.py
@@ -4,16 +4,38 @@ import os
 from pathlib import Path
 import subprocess
 
-this_dir = os.path.dirname(os.path.abspath(__file__))
-
-if os.path.isdir(".git"):
-    subprocess.run(["git", "submodule", "update", "--init", "csrc/cutlass"], check=True)
-
-nvcc_flags = ["-std=c++17",
-              "--expt-relaxed-constexpr",
-              "-arch=sm_75",
-              "-O3",
-              "-lineinfo"]
+# this_dir = os.path.dirname(os.path.abspath(__file__))
+#
+# if os.path.isdir(".git"):
+#     subprocess.run(["git", "submodule", "update", "--init", "csrc/cutlass"], check=True)
+#
+# nvcc_flags = ["-std=c++17",
+#               "--expt-relaxed-constexpr",
+#               "-arch=sm_75",
+#               "-O3",
+#               "-lineinfo"]
+#
+# setup(
+#     name="flash_attn_turing",
+#     ext_modules=[
+#         CUDAExtension(
+#             name="flash_attn_turing",
+#             sources=["csrc/flash_attn/flash_api.cpp",
+#                      "csrc/flash_attn/src/flash_fwd_kernel.cu"
+#                      ],
+#             #include_dirs=cutlass_include_dirs,
+#             include_dirs=[
+#                 Path(this_dir) / "csrc" / "flash_attn",
+#                 Path(this_dir) / "csrc" / "flash_attn" / "src",
+#                 Path(this_dir) / "csrc" / "cutlass" / "include",
+#                 Path(this_dir) / "csrc" / "cutlass" / "tools/util/include"
+#                 ],
+#             extra_compile_args={'nvcc': nvcc_flags}
+#         )
+#     ],
+#     install_requires=["torch"],
+#     cmdclass={"build_ext": BuildExtension}
+# )
 
 setup(
     name="flash_attn_turing",
@@ -23,16 +45,8 @@ setup(
             sources=["csrc/flash_attn/flash_api.cpp",
                      "csrc/flash_attn/src/flash_fwd_kernel.cu"
                      ],
-            #include_dirs=cutlass_include_dirs,
-            include_dirs=[
-                Path(this_dir) / "csrc" / "flash_attn",
-                Path(this_dir) / "csrc" / "flash_attn" / "src",
-                Path(this_dir) / "csrc" / "cutlass" / "include",
-                Path(this_dir) / "csrc" / "cutlass" / "tools/util/include"
-                ],
             extra_compile_args={'nvcc': nvcc_flags}
         )
     ],
     install_requires=["torch"],
     cmdclass={"build_ext": BuildExtension}
-)
