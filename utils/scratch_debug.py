3d97c3610a2f1d7865ed8260a9f68ce01b45981a
steve <stevesiu1013@gmail.com>
2025-04-06 19:37:51 +1000

    fix

--- utils/scratch_debug.py
+++ utils/scratch_debug.py
@@ -8,7 +8,6 @@ import argparse
 q = torch.randn(2,3, dtype=torch.float16).to("cuda")
 k = torch.randn(2,3, dtype=torch.float16).to("cuda")
 v = torch.randn(2,3, dtype=torch.float16).to("cuda")
-a = torch.randn(2,3, dtype=torch.float16).to("cuda")
 print("hi")
 # a, b = flash_attn_func(q,k,v,1,1,1,1)
 
