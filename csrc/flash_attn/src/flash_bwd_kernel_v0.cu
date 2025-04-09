f1d8a186adc25dd47dab44b230db6dd868f788cd
steve <stevesiu1013@gmail.com>
2025-04-09 21:59:07 +1000

    fix

--- csrc/flash_attn/src/flash_bwd_kernel_v0.cu
+++ csrc/flash_attn/src/flash_bwd_kernel_v0.cu
@@ -212,12 +212,12 @@ void compute_dq_dk_dv_kernel_v0(
 
     Tensor tdVrdV = make_tensor(make_rmem_ptr<half_t>(&frag), tdVrdV_float.layout());
 
-    copy(tdVrdV, tdVsdV);
-    //copy(tdVsdV, tdVgV);
+//    copy(tdVrdV, tdVsdV);
+    copy(tdVrdV, tdVgV);
 
-    dq_ptr[0] = static_cast<half_t>(0.0f);
-    dk_ptr[0] = static_cast<half_t>(0.0f);
-    dv_ptr[0] = static_cast<half_t>(0.0f);
+//     dq_ptr[0] = static_cast<half_t>(0.0f);
+//     dk_ptr[0] = static_cast<half_t>(0.0f);
+//     dv_ptr[0] = static_cast<half_t>(0.0f);
 
     if (thread0()) {
         print(gQ);
