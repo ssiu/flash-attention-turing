e508e7474ba91c49a6c626fbec751e804e1cb306
steve <stevesiu1013@gmail.com>
2025-04-10 23:32:23 +1000

    fix

--- csrc/flash_attn/src/flash_bwd_kernel_v0.cu
+++ csrc/flash_attn/src/flash_bwd_kernel_v0.cu
@@ -158,21 +158,21 @@ void compute_dq_dk_dv_kernel_v0(
 
     // load K, V, dK, dV tiles
     copy(tSgK, tSsK);
-    if (thread0()) {
-        print(gdOt);
-        print("\n");
-//         for (int i =0; i<128;i++) {
-//             printf("%f\n", static_cast<float>(gdOt(i,0,0)));
-//         }
-//         for (int i =0; i< 16;i++) {
-//             printf("%f\n", static_cast<float>(tdVgdOt(0,i,0,0)));
-//         }
-        for (int i =0; i< tdVrdOt.size();i++) {
-            printf("%f\n", static_cast<float>(tdVrdOt[i]));
-        }
-//         print(tdVgdOt); //(_2,_16,_4,4)
+//     if (thread0()) {
+//         print(gdOt);
 //         print("\n");
-    }
+// //         for (int i =0; i<128;i++) {
+// //             printf("%f\n", static_cast<float>(gdOt(i,0,0)));
+// //         }
+// //         for (int i =0; i< 16;i++) {
+// //             printf("%f\n", static_cast<float>(tdVgdOt(0,i,0,0)));
+// //         }
+//         for (int i =0; i< tdVrdOt.size();i++) {
+//             printf("%f\n", static_cast<float>(tdVrdOt[i]));
+//         }
+// //         print(tdVgdOt); //(_2,_16,_4,4)
+// //         print("\n");
+//     }
 
     clear(tdVrdV_float);
 
