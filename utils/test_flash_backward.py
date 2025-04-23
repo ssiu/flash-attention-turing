ffcae8ccce8acd5a61b2133adb178050eb89c1cb
steve <stevesiu1013@gmail.com>
2025-04-23 21:22:48 +1000

    fix

--- utils/test_flash_backward.py
+++ utils/test_flash_backward.py
@@ -135,6 +135,15 @@ def main():
     print(f"sum_error = {sum_error}, avg_error = {avg_error}, max_error = {max_error},\nmax_error output = {output_value}, max_error output torch = {output_torch_value}")
 
 
+    print("==========")
+
+    print("Comparing dQ")
+
+    sum_error, avg_error, max_error, output_value, output_torch_value = \
+        get_error(d_query, d_query_torch, batch_size, seq_len, num_heads, head_dim)
+
+    print(f"sum_error = {sum_error}, avg_error = {avg_error}, max_error = {max_error},\nmax_error output = {output_value}, max_error output torch = {output_torch_value}")
+
     # for i in range(128):
     #     print(f"i = {i}, dK = {d_key[0,0,0,i]}, dK_torch = {d_key_torch[0,0,0,i]}")
     #
