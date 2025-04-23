62f77ce1f32f00aff203f2950f421b45ff685a05
steve <stevesiu1013@gmail.com>
2025-04-23 21:44:21 +1000

    fix

--- profile.sh
+++ profile.sh
@@ -14,7 +14,7 @@ pip install .
 
 ncu -f --target-processes all --set full \
 --import-source on \
--o profile_flash_attn python utils/test_flash_backward.py 1 128 1 128
+-o profile_flash_attn python utils/test_flash_backward.py 4 4096 32 128
 
 
 echo "All done!"
