03bce5d429e85b84e253a11cf0b243add5628d8b
steve <stevesiu1013@gmail.com>
2025-04-23 14:00:18 +1000

    fix

--- csrc/flash_attn/src/flash_bwd_kernel_v1.cu
+++ csrc/flash_attn/src/flash_bwd_kernel_v1.cu
@@ -291,13 +291,6 @@ void compute_dq_dk_dv_kernel_v1(
                            make_coord(blockIdx.z, 0));
 
 
-    // dQ
-    Tensor mdQ = make_tensor(make_gmem_ptr(dq_ptr),
-                            make_shape(batch_size, seq_len, num_heads, head_dim),
-                            make_stride(seq_len * num_heads * head_dim, num_heads * head_dim, head_dim, Int<1>{}));
-
-    Tensor gdQ = local_tile(mdQ(blockIdx.x, _, blockIdx.y, _), Shape<Int<kBlockM>, Int<kHeadDim>>{},
-                           make_coord(_, 0));
 
 
     extern __shared__ char smem_[];
