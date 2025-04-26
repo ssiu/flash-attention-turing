633e36c3b4e53d4af0f584d53fa5200667727055
steve <stevesiu1013@gmail.com>
2025-04-26 23:47:58 +1000

    fix

--- csrc/flash_attn/src/flash_bwd_kernel_v3.cu
+++ csrc/flash_attn/src/flash_bwd_kernel_v3.cu
@@ -530,12 +530,12 @@ void compute_dq_dk_dv_kernel_v3(
 //
 //
 //         //convert P from fp32 to fp16
-        constexpr int num_element = decltype(size(tSrP_float))::value;
-
-        cutlass::NumericArrayConverter<half_t, float, num_element> convert_op;
-        auto frag = convert_op(*reinterpret_cast<const cutlass::Array<float, num_element> *>(tSrP_float.data()));
-
-        Tensor tSrP = make_tensor(make_rmem_ptr<half_t>(&frag), tSrP_float.layout()); */
+//         constexpr int num_element = decltype(size(tSrP_float))::value;
+//
+//         cutlass::NumericArrayConverter<half_t, float, num_element> convert_op;
+//         auto frag = convert_op(*reinterpret_cast<const cutlass::Array<float, num_element> *>(tSrP_float.data()));
+//
+//         Tensor tSrP = make_tensor(make_rmem_ptr<half_t>(&frag), tSrP_float.layout());
 //
 //
 //         // convert dS from fp32 to fp16
