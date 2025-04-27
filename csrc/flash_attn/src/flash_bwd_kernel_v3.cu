059423abe80a55a2b393e413565144e08a9c44fb
steve <stevesiu1013@gmail.com>
2025-04-27 21:19:45 +1000

    fix

--- csrc/flash_attn/src/flash_bwd_kernel_v3.cu
+++ csrc/flash_attn/src/flash_bwd_kernel_v3.cu
@@ -393,13 +393,14 @@ void compute_dq_dk_dv_kernel_v3(
 
     clear(tdVrdV_float);
     clear(tdKrdK_float);
-    clear(tdQrdQ_float);
+
     CUTE_NO_UNROLL
     for (int q_tile = 0; q_tile < Q_TILE_MAX; ++q_tile) {
         copy(gmem_tiled_copy_QKV, tVrV, tVsV);
 
         clear(tSrS_float);
         clear(tdPrdP_float);
+        clear(tdQrdQ_float);
 
         // load gQ to sQ
 //         copy(tSgQ(_,_,_,q_tile), tSsQ);
