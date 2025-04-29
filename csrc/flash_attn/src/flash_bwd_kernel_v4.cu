141d76aefd57e0e530bcb7e0dae9d86d7b21f259
steve <stevesiu1013@gmail.com>
2025-04-29 15:00:16 +1000

    fix

--- csrc/flash_attn/src/flash_bwd_kernel_v4.cu
+++ csrc/flash_attn/src/flash_bwd_kernel_v4.cu
@@ -428,10 +428,10 @@ void compute_dq_kernel_v4(
 //             copy(smem_tiled_copy_K, tSsK_copy_view(_,_,qk_block), tSrK_copy_view(_,_,qk_block));
             copy(tSsQ(_,_,qk_block), tSrQ(_,_,qk_block));
             copy(tSsK(_,_,qk_block), tSrK(_,_,qk_block));
-            if (thread0() && kv_tile==0 && qk_block==0) {
-                print_tensor(tSrK);
-            }
-            //gemm(tiled_mma_S, tSrQ(_,_,qk_block), tSrK(_,_,qk_block), tSrS_float);
+//             if (thread0() && kv_tile==0 && qk_block==0) {
+//                 print_tensor(tSrK);
+//             }
+            gemm(tiled_mma_S, tSrQ(_,_,qk_block), tSrK(_,_,qk_block), tSrS_float);
         }
 
 //         if (thread0()) {
