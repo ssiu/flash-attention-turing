e68281351d8186b24f06ea05ef2bcda9eb24e67f
steve <stevesiu1013@gmail.com>
2025-05-03 13:43:43 +1000

    fix

--- csrc/flash_attn/src/flash_bwd_kernel_v6.cu
+++ csrc/flash_attn/src/flash_bwd_kernel_v6.cu
@@ -1255,19 +1255,6 @@ flash_bwd_v6(torch::Tensor q,
 
     int maxbytes = 65536;
 
-    // compute dQ
-    dim3 dimGrid_dq(batch_size, num_heads, seq_len / kBlockM);
-    dim3 dimBlock_dq(256);
-
-    cudaFuncSetAttribute(compute_dq_kernel_v6, cudaFuncAttributeMaxDynamicSharedMemorySize, maxbytes);
-    compute_dq_kernel_v6<<<dimGrid_dq, dimBlock_dq, maxbytes>>>(q_ptr,
-                                            k_ptr,
-                                            v_ptr,
-                                            l_ptr,
-                                            d_ptr,
-                                            do_ptr,
-                                            dq_ptr,
-                                            batch_size, seq_len, num_heads, head_dim);
 
 
 
@@ -1285,6 +1272,19 @@ flash_bwd_v6(torch::Tensor q,
                                             do_ptr,
                                             dk_ptr,
                                             dv_ptr,
+                                            batch_size, seq_len, num_heads, head_dim);
+    // compute dQ
+    dim3 dimGrid_dq(batch_size, num_heads, seq_len / kBlockM);
+    dim3 dimBlock_dq(256);
+
+    cudaFuncSetAttribute(compute_dq_kernel_v6, cudaFuncAttributeMaxDynamicSharedMemorySize, maxbytes);
+    compute_dq_kernel_v6<<<dimGrid_dq, dimBlock_dq, maxbytes>>>(q_ptr,
+                                            k_ptr,
+                                            v_ptr,
+                                            l_ptr,
+                                            d_ptr,
+                                            do_ptr,
+                                            dq_ptr,
                                             batch_size, seq_len, num_heads, head_dim);
 
     return { dq, dk, dv };
