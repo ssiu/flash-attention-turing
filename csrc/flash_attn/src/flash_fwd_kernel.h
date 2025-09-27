0652493b1942f6907d6293de1da92d4590f984c4
steve <stevesiu1013@gmail.com>
2025-09-27 21:30:07 +1000

    fix

--- csrc/flash_attn/src/flash_fwd_kernel.h
+++ csrc/flash_attn/src/flash_fwd_kernel.h
@@ -483,7 +483,7 @@ inline __device__ void compute_attn_1rowblock(half_t* __restrict__ q,
             tSrS_float, warp_id, lane_id, n_block, n_block_max, kBlockN
         );
 
-        if (seqlen_q == 128 && seqlen_k == 128 && threadIdx.x == 0 && threadIdx.x == 0) {
+        if (seqlen_q == 128 && seqlen_k == 128 && blockIdx.x == 0 && blockIdx.y == 0 && blockIdx.z == 0 && threadIdx.x == 0) {
             printf("is_causal is %d, tSrS_float is %f\n", is_casual, tSrS_float(make_coord(0,0),0,0));
             //print_tensor(tSrS_float);
         }
