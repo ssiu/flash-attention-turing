3711ea6bbb3e9a7f1264a8c4a35259347990037b
steve <stevesiu1013@gmail.com>
2026-03-20 23:31:07 +1100

    fix

--- csrc/flash_attn/src/flash_fwd_kernel.h
+++ csrc/flash_attn/src/flash_fwd_kernel.h
@@ -348,13 +348,15 @@ inline __device__ void compute_attn_1rowblock(
 
 
         // compute P = softmax(S)
-        for (int i =0; i<2; i++) {
-            for (int j=0; j < tSrS_float(make_coord(_,i),_,_).size(); j++) {                
-                tSrS_float(make_coord(_,i),_,_)[j] = expf(tSrS_float(make_coord(_,i),_,_)[j] - rM[i]);         
-
+         for (int i =0; i<2; i++) {
+            for (int j=0; j < tSrS_float(make_coord(_,i),_,_).size(); j++) {
+                if (rM[i] == -FLT_MAX) {
+                    tSrS_float(make_coord(_,i),_,_)[j] = 0.0f;
+                } else {
+                    tSrS_float(make_coord(_,i),_,_)[j] = expf(tSrS_float(make_coord(_,i),_,_)[j] - rM[i]);
+                }
             }
         }
-
         // rescale l and also reset rD to 0
         for (int i =0; i<2; i++) {
             rL[i] = expf(rM_old[i] - rM[i]) * rL_old[i];
